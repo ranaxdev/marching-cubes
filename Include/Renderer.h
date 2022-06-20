46bdb318387f87658f112f6d6964305dfc58d848
ranaxdev <sharrq1@gmail.com>
2022-06-20 09:25:01 +0100

    Update

---
+++ Include/Renderer.h
@@ -0,0 +1,49 @@
+#ifndef _H_RENDERER
+#define _H_RENDERER
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <vector>
+
+#include "Globals.h"
+#include "Shader.h"
+#include "Menu.h"
+
+class Renderer{
+public:
+    Renderer(GLuint& VAO, GLuint buf[]);
+    ~Renderer() = default;
+
+    void enableAxis();
+
+
+    void renderGUI(Menu& g);
+    void renderAxis();
+
+    unsigned int prepBuf(GLfloat data[], GLuint size);
+    unsigned int prepBuf(GLushort data[], GLuint size);
+    unsigned int prepBuf(std::vector<GLfloat>& data, bool big);
+    unsigned int editBuf(std::vector<GLfloat>& data, GLuint i);
+
+    void formatBuf(GLuint loc, GLint comps_per_elem, std::vector<int> attribs, Shader& s);
+
+private:
+    // Memory mgmt
+    static int free_buf;
+    static int free_bindpoint;
+    GLuint VAO;
+    GLuint* buf;
+    GLuint fluid_buffer;
+
+    // Shaders
+    // REMINDER: change environment variable SRC in globals to setup shaders correctly
+    Shader shader_axis = Shader(SRC+"Shaders/vert.glsl", SRC+"Shaders/frag.glsl");
+
+    // Colors
+    glm::vec4 red = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::vec4 cyan = glm::vec4(0.0f, 1.0f, 1.0f, 1.0f);
+
+
+};
+#endif
---
+++ Source/Renderer.cpp
@@ -0,0 +1,172 @@
+#include <string>
+#include <iostream>
+#include "Logger.h"
+#include "Harness.h"
+#include "Renderer.h"
+
+// Static initializations
+int Renderer::free_buf          = -1;
+int Renderer::free_bindpoint    = -1;
+
+Renderer::Renderer(GLuint &VAO, GLuint *buf)
+: VAO(VAO), buf(buf) {
+
+
+}
+
+/* ****************************************************************
+ *                  RENDER-ABLES INITIALIZATIONS                  *
+ ******************************************************************/
+/*
+ * Initialize axes data
+ */
+void Renderer::enableAxis() {
+    const GLfloat axis_data[] = {
+            // Axis 1 (red)
+            0.0f, 0.0f, 2.5f,     1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, -2.5f,    1.0f, 0.0f, 0.0f,
+            // Axis 2 (green)
+            0.0f, 2.5f, 0.0f,    0.0f, 1.0f, 0.0f,
+            0.0f, -2.5f, 0.0f,   0.0f, 1.0f, 0.0f,
+            // Axis 3 (blue)
+            2.5f, 0.0f, 0.0f,    0.0f, 0.0f, 1.0f,
+            -2.5f, 0.0f, 0.0f,   0.0f, 0.0f, 1.0f
+    };
+
+    // Prepare buffer
+    GLuint loc = prepBuf((GLfloat*)axis_data, sizeof(axis_data));
+
+    // Format data
+    formatBuf(loc, 3, {0, 1}, Renderer::shader_axis);
+
+}
+
+
+
+
+/* ****************************************************************
+ *                        RENDERING ROUTINES                      *
+ ******************************************************************/
+// temp - just to get oriented in the world real quick
+void Renderer::renderAxis() {
+    shader_axis.bind();
+    shader_axis.setMat4(20, qaiser::Harness::VP);
+    glLineWidth(3.0f);
+
+    glDrawArrays(GL_LINES , 0, 6);
+}
+
+
+
+void Renderer::renderGUI(Menu &g) {
+    g.update();
+
+}
+
+
+/* ****************************************************************
+ *                      BUFFER SETUP ROUTINES                     *
+ ******************************************************************/
+/*
+ * @data - Array of float data
+ * @size - Size of the array in bytes
+ * Inits a new buffer and returns its index
+ */
+unsigned int Renderer::prepBuf(GLfloat *data, GLuint size) {
+    free_buf++;
+    glCreateBuffers(1, &buf[free_buf]);
+    glNamedBufferStorage(buf[free_buf], size, data, GL_MAP_READ_BIT|GL_MAP_WRITE_BIT);
+
+    return free_buf;
+}
+unsigned int Renderer::prepBuf(GLushort *data, GLuint size) {
+    free_buf++;
+    glCreateBuffers(1, &buf[free_buf]);
+    glNamedBufferStorage(buf[free_buf], size, data, GL_MAP_READ_BIT|GL_MAP_WRITE_BIT);
+
+    return free_buf;
+}
+
+
+/*
+ * @data - List of float data
+ * Inits a new buffer and returns its index
+ */
+unsigned int Renderer::prepBuf(std::vector<GLfloat>& data, bool big) {
+    int size = (int) data.size();
+    int dat_size = 4*size;
+
+    // Either allocate a big buffer (for large meshes and such)
+    // Or just as large as your data
+    int to_allocate;
+    if(big)
+        to_allocate = ONE_MB;
+    else
+        to_allocate = dat_size;
+
+
+    free_buf++;
+    glCreateBuffers(1, &buf[free_buf]);
+    glNamedBufferStorage(buf[free_buf], to_allocate, nullptr, GL_MAP_READ_BIT|GL_MAP_WRITE_BIT); // 1mb buffer
+
+    float* ptr = (float*) glMapNamedBufferRange(buf[free_buf], 0, dat_size, GL_MAP_READ_BIT|GL_MAP_WRITE_BIT);
+    for(int i=0; i<size; i++){
+        ptr[i] = data[i];
+    }
+    glUnmapNamedBuffer(buf[free_buf]);
+
+    return free_buf;
+}
+
+/*
+ * @buf - Buffer to edit
+ * Edits buffer data
+ */
+unsigned int Renderer::editBuf(std::vector<GLfloat>& data, GLuint i) {
+    int size = (int) data.size();
+    int dat_size = 4*size;
+
+    // Buffer overflowed
+    if(dat_size >= ONE_MB){
+        Logger::log(ERROR, "Buffer overflowed, buffer ID: "+ std::to_string(i), __FILENAME__);
+    }
+
+    float* ptr = (float*) glMapNamedBufferRange(buf[i], 0, dat_size, GL_MAP_READ_BIT|GL_MAP_WRITE_BIT);
+    for(int x=0; x<size; x++){
+        ptr[x] = data[x];
+    }
+    glUnmapNamedBuffer(buf[i]);
+
+    return i;
+}
+
+
+/*
+ * @loc                     - Active buffer location
+ * @comps_per_elem          - Number of components in an element
+ * @names                   - List of attribute names used in GLSL
+ * @s                       - Shader to get the attribute names from
+ *
+ * Formats the buffer for the VAO
+ */
+
+void Renderer::formatBuf(GLuint loc, GLint comps_per_elem, std::vector<int> attribs, Shader& s) {
+    free_bindpoint++;
+    auto num_attribs = attribs.size();
+
+    for(int i=0; i < num_attribs; i++){
+
+        glVertexArrayAttribFormat(VAO, attribs[i], comps_per_elem, GL_FLOAT, GL_FALSE, (i*comps_per_elem)*sizeof(float));
+        glVertexArrayAttribBinding(VAO, attribs[i], free_bindpoint);
+        glEnableVertexArrayAttrib(VAO, attribs[i]);
+    }
+
+    glVertexArrayVertexBuffer(VAO, free_bindpoint, buf[loc], 0, (num_attribs*comps_per_elem)*sizeof(float));
+}
+
+
+
+
+
+
+
